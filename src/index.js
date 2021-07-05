06bc4767c37613fb544c34f2d0e21ea408fac7e8
Francois Bruere <francois.bruere@gmail.com>
2021-07-05 11:28:09 +0200

    InlineStylingJSX

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ const lname = "Bruere";
 const num = 7;
 const year = new Date().getFullYear();
 
+const customStyle = {
+  color: "red",
+  fontSize: "10px"
+};
+
 ReactDOM.render(
   <div>
-    <h1>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
       Hello {fname} {lname}!
     </h1>
-    <p>
+    <p style={customStyle}>
       My lucky number is {3 + 4} or const {num}.
     </p>
+    <div></div>
     <p> Copyright {year} </p>
   </div>,
   document.getElementById("root")
